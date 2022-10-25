5480c912d760a607020d6348dc6b1a610857ba63
ZedayKayo <zeday.kayo@gmail.com>
2022-10-25 01:19:46 +0100

    g

--- om_dup_contact/models/contact.py
+++ om_dup_contact/models/contact.py
@@ -1,8 +1,8 @@
 from odoo import models,fields
 
 class ResPartner(models.Model):
-    _inherit=['res.partner']
-    
+    _inherit= 'res.partner'
+
     con_prenom = fields.Char(string="Prénom")
     con_age = fields.Char(string="Age")
     con_n_cin = fields.Char(string="N° CIN")
